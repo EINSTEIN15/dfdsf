fc49239cc74a76a9e45964a36a1cf82887d087a4
nknipfe2 <nknipfe2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-08-26 01:42:01 +0000

    lab_intro submission

---
+++ nknipfe2/lab_intro/rgbapixel.h
@@ -0,0 +1,38 @@
+#ifndef _RGBAPIXEL_H
+#define _RGBAPIXEL_H
+
+
+#include <cstdint>
+using std::uint8_t;
+
+class RGBAPixel
+{
+ public:
+  RGBAPixel();
+  RGBAPixel(uint8_t red, uint8_t green, uint8_t blue);
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  uint8_t alpha;
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
