0626a2e3fb401ba8445e7bda89191b3d832cf234
nknipfe2 <nknipfe2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-09-09 17:48:53 +0000

    mp2.1 submission

---
+++ mp2/image.h
@@ -0,0 +1,32 @@
+#ifndef _IMAGE_H
+#define _IMAGE_H
+
+#include <cstdint>
+#include <iostream>
+#include "png.h"
+using std::uint8_t;
+class Image:public PNG
+{
+ public:
+
+  void flipleft();
+  void adjustbrightness (int r, int g, int b);
+  void invertcolors();
+  RGBAPixel * ptr1= new RGBAPixel(); 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+#endif
