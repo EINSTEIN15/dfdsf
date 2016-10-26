d780de23849564b156365840a985c435a0fc72c5
nknipfe2 <nknipfe2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-10-26 00:39:18 +0000

    mp5.1 submission

--- mp5/main.cpp
+++ mp5/main.cpp
@@ -29,14 +29,14 @@ int main()
 
     // test pruneSize and idealPrune (slow in valgrind, so you may want to
     // comment these out when doing most of your testing for memory leaks)
-    cout << "fullTree.pruneSize(0) = "      << fullTree.pruneSize(0) << endl;
+    /*    cout << "fullTree.pruneSize(0) = "      << fullTree.pruneSize(0) << endl;
     cout << "fullTree.pruneSize(100) = "    << fullTree.pruneSize(100) << endl;
     cout << "fullTree.pruneSize(1000) = "   << fullTree.pruneSize(1000) << endl;
     cout << "fullTree.pruneSize(100000) = " << fullTree.pruneSize(100000) << endl;
 
     cout << "fullTree.idealPrune(1000) = "  << fullTree.idealPrune(1000) << endl;
     cout << "fullTree.idealPrune(10000) = " << fullTree.idealPrune(10000) << endl;
-
+    */
     // Test some creation/deletion functions
     Quadtree fullTree2;
     fullTree2 = fullTree;
@@ -44,30 +44,30 @@ int main()
     imgOut.writeToFile("outCopy.png");
 
     // test clockwiseRotate
-    fullTree.clockwiseRotate();
+    //    fullTree.clockwiseRotate();
     imgOut = fullTree.decompress();
     imgOut.writeToFile("outRotated.png");
 
     // test prune
     fullTree = fullTree2;
-    fullTree.prune(1000);
+    // fullTree.prune(1000);
     imgOut = fullTree.decompress();
     imgOut.writeToFile("outPruned.png");
 
     // test several functions in succession
     Quadtree fullTree3(fullTree2);
-    fullTree3.clockwiseRotate();
-    fullTree3.prune(10000);
-    fullTree3.clockwiseRotate();
-    fullTree3.clockwiseRotate();
-    fullTree3.clockwiseRotate();
+    // fullTree3.clockwiseRotate();
+    // fullTree3.prune(10000);
+    //fullTree3.clockwiseRotate();
+    //    fullTree3.clockwiseRotate();
+    // fullTree3.clockwiseRotate();
     imgOut = fullTree3.decompress();
     imgOut.writeToFile("outEtc.png");
 
     // ensure that printTree still works
     Quadtree tinyTree(imgIn, 32);
     cout << "Printing tinyTree:\n";
-    tinyTree.prune(100);
+    // tinyTree.prune(100);
     tinyTree.printTree();
 
     return 0;
