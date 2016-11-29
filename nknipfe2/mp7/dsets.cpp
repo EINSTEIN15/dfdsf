07fff36946a0de6aff15f12f5cc126dace6ed46a
nknipfe2 <nknipfe2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-11-29 15:42:53 +0000

    mp7.1 submission

--- nknipfe2/mp7/dsets.cpp
+++ nknipfe2/mp7/dsets.cpp
@@ -1,1 +1,52 @@
 /* Your code here! */
+#include "dsets.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+void DisjointSets::addelements	(int num)
+{
+  for(int i=0;i<num;i++)
+    {
+      myvector.push_back(-1);
+    }
+}
+
+int DisjointSets::find	(int elem)	
+{
+  if(myvector[elem]<0)
+    {
+      return elem;
+    }
+  else
+    {
+
+      return myvector[elem]=find(myvector[elem]);
+
+    }
+}
+void DisjointSets::setunion	(int a,int b)		
+{
+  int x=find(a);
+  int y=find(b);
+  
+
+  int newSize=myvector[x]+myvector[y];
+  if(myvector[x]<=myvector[y])
+    {
+      myvector[y]=x;
+      myvector[x]=newSize;
+
+    }
+  else
+    {
+      myvector[x]=y;
+      myvector[y]=newSize;
+
+
+    }
+
+}
+
