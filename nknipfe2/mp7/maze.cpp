f0b4f12cac9878da31e7931a68bf1b5f9e59dc07
nknipfe2 <nknipfe2@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-12-07 04:24:41 +0000

    mp7 submission

--- nknipfe2/mp7/maze.cpp
+++ nknipfe2/mp7/maze.cpp
@@ -1,1 +1,75 @@
 /* Your code here! */
+#include "maze.h"
+#include <vector>
+#include <iostream>
+#include "dsets.cpp"
+using namespace std;
+
+SquareMaze::SquareMaze ()
+{
+
+}
+void SquareMaze::makeMaze(int width, int height)
+{
+  this->mazeheight=height;
+  this->mazewidth=width;
+  DisjointSets set;
+  set.addelements(height*width);
+
+
+
+
+
+
+
+
+
+}
+bool SquareMaze::canTravel(int x,int y,int dir) const
+{
+
+
+
+}
+
+
+void SquareMaze::setWall(int x,int y, int dir, bool exists)
+{
+  int tmp=y*mazewidth+x;
+  if(dir==0)
+    {
+      rightwall[tmp]=exists;
+      
+    }
+  else if(dir==1)
+    {
+      bottomwall[tmp]=exists;
+      
+    }
+
+
+}
+
+
+vector<int> SquareMaze::solveMaze()
+{
+
+
+
+}
+
+
+PNG *SquareMaze:: drawMaze () const
+{
+
+
+
+
+}
+PNG* SquareMaze:: drawMazeWithSolution()
+{
+
+
+
+
+}
--- nknipfe2/mp7/maze.h
+++ nknipfe2/mp7/maze.h
@@ -1,1 +1,25 @@
 /* Your code here! */
+#ifndef MAZE_H
+#define MAZE_H
+#include <vector>
+#include <iostream>
+#include "png.cpp"
+using namespace std;
+class SquareMaze
+{
+ public:
+  SquareMaze();
+  void makeMaze(int width,int height);
+  bool canTravel(int x,int y,int dir) const;
+  void setWall(int x,int y, int dir, bool exists);
+  vector<int> solveMaze();
+  PNG* drawMaze() const;
+  PNG* drawMazeWithSolution();
+ private:
+  vector<int> v;
+  vector<bool>rightwall;
+  vector<bool>leftwall;
+  int mazewidth;
+  int mazeheight;
+};
+#endif
